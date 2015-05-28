cb585f35eaf7763909b0117f4522c5c4b188e1d0
Angelo Dini <angelo.dini@divio.ch>
2015-05-28 15:15:29 +0200

    added segmentation

--- dev/local_settings.py
+++ dev/local_settings.py
@@ -2,6 +2,11 @@ from settings import *
 # override settings as required
 
 INSTALLED_APPS += [
-    'aldryn_blueprint',
+#    'aldryn_blueprint',
     'aldryn_segmentation',
+    'country_segment',
+]
+
+MIDDLEWARE_CLASSES += [
+    'country_segment.middleware.ResolveCountryCodeMiddleware'
 ]
