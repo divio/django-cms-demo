28ecca62a445de9ad8f3f1e8eb8b22d8da62c148
Angelo Dini <angelo.dini@divio.ch>
2015-06-05 23:36:08 +0100

    fix code if DEBUG is set to True

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- src/urls.py
+++ src/urls.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = i18n_patterns('',
+urlpatterns = patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views.serve', { 'insecure': True }),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+) + i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
