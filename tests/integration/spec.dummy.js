17408ed50d70848edc012e02a7f2925d1e772317
Vadim Sikora <vadim.sikora@divio.ch>
2015-07-01 10:12:09 +0200

    cleanup dummy test

--- tests/integration/spec.dummy.js
+++ tests/integration/spec.dummy.js
@@ -1,7 +1,7 @@
-describe('Protractor Demo App', function() {
+describe('Django CMS website', function() {
     it('should have a title', function() {
         browser.get('http://www.django-cms.org');
 
-        expect(browser.getTitle()).toEqual('The easiest way to build and manage your Django projects - django-cms.org');
+        expect(browser.getTitle()).toContain('django-cms');
     });
 });
