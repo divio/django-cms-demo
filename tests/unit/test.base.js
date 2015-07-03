1ee67229184361227655c41394dc09290798c16c
Angelo Dini <angelo.dini@divio.ch>
2015-07-03 18:46:39 +0200

    adapt test infra

--- tests/unit/test.base.js
+++ tests/unit/test.base.js
@@ -3,36 +3,29 @@
  * @copyright: http://www.divio.ch
  */
 
-// #####################################################################################################################
-// #NAMESPACES#
-// var Cl = window.Cl || {};
+'use strict';
 /* global describe, it, expect */
 
 // #####################################################################################################################
 // #TESTS#
-(function () {
-    'use strict';
-
-    describe('base.js', function () {
-        beforeEach(function () {
-            fixture.setBase('tests/fixtures');
-            this.markup = fixture.load('outdatedBrowser.html');
-        });
-
-        it('loads Cl.Utils._document()', function () {
-            expect(window.Cl).toBeDefined();
-            expect(Cl.Utils).toBeDefined();
-        });
-
-        it('loads outdatedBrowser', function () {
-            expect(outdatedBrowser).toBeDefined();
-        });
-
-        it('loads Cl.Debug', function () {
-            expect(Cl.Debug).toBeDefined();
-            new Cl.Debug();
-            expect($('#divio-dt').length).toBeTruthy();
-        });
+describe('base.js', function () {
+    beforeEach(function () {
+        fixture.setBase('tests/fixtures');
+        this.markup = fixture.load('outdatedBrowser.html');
     });
 
-})();
+    it('loads Cl.Utils._document()', function () {
+        expect(window.Cl).toBeDefined();
+        expect(Cl.Utils).toBeDefined();
+    });
+
+    it('loads outdatedBrowser', function () {
+        expect(outdatedBrowser).toBeDefined();
+    });
+
+    it('loads Cl.Debug', function () {
+        expect(Cl.Debug).toBeDefined();
+        new Cl.Debug();
+        expect($('#divio-dt').length).toBeTruthy();
+    });
+});
