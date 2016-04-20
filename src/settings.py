1ed5673bfec3aaf174914852026a23afdebaa76a
Eleanor Berger <eleanor.berger@divio.com>
2016-04-20 17:36:12 +0200

    Install CMS Plugin Filer

--- src/settings.py
+++ src/settings.py
@@ -92,6 +92,12 @@ INSTALLED_APPS = (
     'aldryn_forms',
     'aldryn_forms.contrib.email_notifications',
     'aldryn_locations',
+    'cmsplugin_filer_file',
+    'cmsplugin_filer_folder',
+    'cmsplugin_filer_link',
+    'cmsplugin_filer_image',
+    'cmsplugin_filer_teaser',
+    'cmsplugin_filer_video',
 )
 
 MIDDLEWARE_CLASSES = (
